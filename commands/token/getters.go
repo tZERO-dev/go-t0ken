20ac766f80e793f34815cb630a6603715d9a63a2
Justin Wilson <justin@minty.io>
2019-02-08 10:31:53 -0700

    Added a few missing getters

--- commands/token/getters.go
+++ commands/token/getters.go
@@ -25,21 +25,32 @@ var GetterCommands = []*cobra.Command{
 		Run:   func(cmd *cobra.Command, args []string) { cmd.Println(erc20.T0kenBin) },
 	},
 	&cobra.Command{
-		Use:    "owner",
-		Short:  "Gets the owner address of the t0ken",
-		Args:   cobra.NoArgs,
+		Use:    "allowance <owner> <spender>",
+		Short:  "Gets the amount of tokens the <owner> has approved the <spender> to transfer",
+		Args:   cli.ChainArgs(cobra.ExactArgs(2), cli.AddressArgFunc(0), cli.AddressArgFunc(1)),
 		PreRun: connectWithT0kenCallerSession,
 		Run: func(cmd *cobra.Command, args []string) {
-			cli.CheckAddressGetter(cmd)(callSession.Owner())
+			owner := common.HexToAddress(args[0])
+			spender := common.HexToAddress(args[1])
+			cli.CheckGetter(cmd)(callSession.Allowance(owner, spender))
 		},
 	},
 	&cobra.Command{
 		Use:   "balanceOf <address>",
-		Short: "gets the balance of the given <address>",
+		Short: "Gets the balance of the given <address>",
 		Args:  cli.ChainArgs(cobra.MaximumNArgs(1), cli.AddressArgFunc(0)),
 		Run: func(cmd *cobra.Command, args []string) {
 			addr := common.HexToAddress(args[0])
 			cli.CheckGetter(cmd)(callSession.BalanceOf(addr))
+		},
+	},
+	&cobra.Command{
+		Use:   "cancellations <address>",
+		Short: "Gets the replacement address of the given <address>, or a zero-address when it has not been cancelled",
+		Args:  cli.ChainArgs(cobra.MaximumNArgs(1), cli.AddressArgFunc(0)),
+		Run: func(cmd *cobra.Command, args []string) {
+			addr := common.HexToAddress(args[0])
+			cli.CheckAddressGetter(cmd)(callSession.Cancellations(addr))
 		},
 	},
 	&cobra.Command{
@@ -59,7 +70,7 @@ var GetterCommands = []*cobra.Command{
 		},
 	},
 	&cobra.Command{
-		Use:   "superseded",
+		Use:   "getSuperseded",
 		Short: "Gets the superseded address of the given address",
 		Args:  cli.ChainArgs(cobra.MaximumNArgs(1), cli.AddressArgFunc(0)),
 		Run: func(cmd *cobra.Command, args []string) {
@@ -70,10 +81,36 @@ var GetterCommands = []*cobra.Command{
 	&cobra.Command{
 		Use:   "holderAt <index>",
 		Short: "Gets the holder address at the given <index>",
-		Args:  cli.IntArgFunc(0),
+		Args:  cli.ChainArgs(cobra.MaximumNArgs(1), cli.IntArgFunc(0)),
 		Run: func(cmd *cobra.Command, args []string) {
 			index, _ := new(big.Int).SetString(args[0], 10)
 			cli.CheckAddressGetter(cmd)(callSession.HolderAt(index))
+		},
+	},
+	&cobra.Command{
+		Use:   "isHolder <address>",
+		Short: "Checks if the given <address> is a current holder",
+		Args:  cli.ChainArgs(cobra.MaximumNArgs(1), cli.AddressArgFunc(0)),
+		Run: func(cmd *cobra.Command, args []string) {
+			addr := common.HexToAddress(args[0])
+			cli.CheckGetter(cmd)(callSession.IsHolder(addr))
+		},
+	},
+	&cobra.Command{
+		Use:   "isLocked",
+		Short: "Checks if the contract is in a locked state",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			cli.CheckGetter(cmd)(callSession.IsLocked())
+		},
+	},
+	&cobra.Command{
+		Use:   "isSuperseded",
+		Short: "Checks if the contract is in a locked state",
+		Args:  cli.ChainArgs(cobra.MaximumNArgs(1), cli.AddressArgFunc(0)),
+		Run: func(cmd *cobra.Command, args []string) {
+			addr := common.HexToAddress(args[0])
+			cli.CheckGetter(cmd)(callSession.IsSuperseded(addr))
 		},
 	},
 	&cobra.Command{
@@ -98,6 +135,24 @@ var GetterCommands = []*cobra.Command{
 		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
 			cli.CheckGetter(cmd)(callSession.Name())
+		},
+	},
+	&cobra.Command{
+		Use:    "owner",
+		Short:  "Gets the owner address of the t0ken",
+		Args:   cobra.NoArgs,
+		PreRun: connectWithT0kenCallerSession,
+		Run: func(cmd *cobra.Command, args []string) {
+			cli.CheckAddressGetter(cmd)(callSession.Owner())
+		},
+	},
+	&cobra.Command{
+		Use:    "shareholders",
+		Short:  "Gets the total number of shareholders",
+		Args:   cobra.NoArgs,
+		PreRun: connectWithT0kenCallerSession,
+		Run: func(cmd *cobra.Command, args []string) {
+			cli.CheckGetter(cmd)(callSession.Shareholders())
 		},
 	},
 	&cobra.Command{
