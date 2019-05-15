8ea180d56f109cbdb36ffda1ecfa7d8585dc4425
Justin Wilson <justin@minty.io>
2019-05-15 12:44:57 -0600

    removed extra repo string

--- xgo.sh
+++ xgo.sh
@@ -7,5 +7,5 @@ xgo \
 	-v \
 	--dest=dist \
 	--targets="darwin/amd64,linux/amd64,windows/amd64" \
-	-ldflags="-X github.com/tzero-dev/go-t0ken/commands.GitCommit=$COMMIT" github.com/tzero-dev/go-t0ken/cmd/t0ken
+	-ldflags="-X github.com/tzero-dev/go-t0ken/commands.GitCommit=$COMMIT" \
 	github.com/tzero-dev/go-t0ken/cmd/t0ken
